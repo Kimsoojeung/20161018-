2a8a02212cd12a6e88583ca3ee9897550b1c1831
gkm2019 <popo2019@naver.com>
2016-10-18 17:46:15 +0900

    20161018두번째

--- 20161018_ksj/src/javabook/ch6/Ch6Ex4.java
+++ 20161018_ksj/src/javabook/ch6/Ch6Ex4.java
@@ -1,8 +1,8 @@
 package javabook.ch6;
 
-import java.util.Scanner;
+//import java.util.Scanner;
 
-public class Ch6Ex4 {
+/*public class Ch6Ex4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -24,4 +24,36 @@ public class Ch6Ex4 {
 
 	}
 
+}*/
+
+/*import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class Ch6Ex4{
+	public static void main(String[] args){
+		Date date1=new Date();
+		Date date2=new Date(1231412547);
+		System.out.println("date1: "+date1);
+		System.out.println("date2: "+date2);
+		
+		System.out.println("date1, date2 compare : "+date1.compareTo(date2));
+		
+		Calendar cal=Calendar.getInstance(TimeZone.getTimeZone("GMT+2"));
+		
+		System.out.println("time : "+cal.get(Calendar.HOUR_OF_DAY)+":"+cal.get(Calendar.MINUTE));
+	}
+}*/
+
+import java.util.Scanner;
+
+public class Ch6Ex4{
+	public static void main(String[] args){
+		Scanner scan=new Scanner(System.in);
+		String msg=scan.next();
+		int num=scan.nextInt();
+		System.out.println("input :"+num);
+	}
+	
 }
+
